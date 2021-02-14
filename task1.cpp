416df3b60aaf2f1e25ecd363b789cb7e275a87d5
Ivan <79052118+GreFort@users.noreply.github.com>
2021-02-15 00:44:24 +0300

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 16;
+    double f;
+    cout << "Const = " << x;
+    f = -1 * sqrt(x);
+    cout << "\nIf x = " << x << " f = " << f;
+    cout << "\nEnter x = ";
+    cin >> x;
+    if (x < 0)
+    {
+        cout << "If x = " << x << " Error, x < 0" << "\n";
+    }
+    else
+    {
+        f = -1 * sqrt(x);
+        cout << "If x = " << x << " f = " << f << "\n";
+    }
+}
+
