07986d8a92507ac790e9908d08676a896ddb0a20
Ivan <79052118+GreFort@users.noreply.github.com>
2021-02-19 20:53:13 +0300

    Add files via upload

---
+++ task6/task6.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+void Function();
+extern double x, f;
+void main()
+{
+    x = 16;
+    cout << "Constant = " << x;
+    Function();
+    cout << "If x = " << x << ", Function = " << f << "\nEnter x = ";
+    cin >> x;
+    if (x < 0)
+    {
+        cout << "If x = " << x << ", Error x<0\n";
+    }
+    else
+    {
+        Function();
+        cout << "If x = " << x << ", Function = " << f << "\n";
+    }
+}
